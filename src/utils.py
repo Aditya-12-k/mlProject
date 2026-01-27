8a925cf0df98a65a8da5b34c6307b658541619ef
Aditya-12-k <Aditya-12-k@users.noreply.github.com>
2026-01-27 20:22:18 +0530

    Hyperparamete done

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,14 @@
 import os
+import warnings
+warnings.filterwarnings("ignore")
+
 import sys
 import pandas as pd
 import numpy as np
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 def save_object(file_path, obj):
     """
     Saves any Python object (model, preprocessor, pipeline, etc) using dill.
@@ -49,13 +52,50 @@ def load_object(file_path):
 #     except Exception as e:
 #         raise CustomException(e,sys)
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+# def evaluate_model(X_train, y_train, X_test, y_test, models,param):
+#     try:
+#         report = {}
+#         for i in range(len(list(models))):
+#             model = list(models.values())[i]
+#             para=param[list(models.keys())[i]]
+#             gs = GridSearchCV(model,para,cv=3)
+#             gs.fit(X_train,y_train)
+#             model.set_params(**gs.best_params_)
+#             model.fit(X_train,y_train)
+#         # for model_name, model in models.items():
+#             # model.fit(X_train, y_train)
+
+#             y_train_pred = model.predict(X_train)
+#             y_test_pred = model.predict(X_test)
+
+#             train_model_score = r2_score(y_train, y_train_pred)
+#             test_model_score = r2_score(y_test, y_test_pred)
+
+#             report[model_name] = test_model_score
+
+#         return report
+
+#     except Exception as e:
+#         raise CustomException(e, sys)
+
+from sklearn.model_selection import GridSearchCV
+
+def evaluate_model(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
         for model_name, model in models.items():
+
+            params = param.get(model_name, {})  # Avoid key error
+
+            gs = GridSearchCV(model, params, cv=3)
+            gs.fit(X_train, y_train)
+
+            # best param set
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
+            # Predictions
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
@@ -68,3 +108,15 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+
+
+
+
+
+
+ 
+
+            
+
+            
